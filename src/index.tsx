c5313b6d4f68942761e5b9d3afa12509efb80c75
Junior Programmer <progphyton@gmail.com>
2023-12-17 13:07:20 +0300

    refactor: template with modern code style

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import { StrictMode } from 'react';
 
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+
+import { App } from './components/App/App';
+import { container } from './constants/container';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
