a20c0d7623669a6385603ea2d6bdd2cf1dc29f65
Junior Programmer <progphyton@gmail.com>
2023-12-17 13:42:28 +0300

    refactor: replace default template with basic layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,11 @@
 import { FC } from 'react';
 
-import './App.css';
-
-import logo from '../../assets/logo.svg';
+import Styles from './App.module.css';
 
 export const App: FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit "App.tsx" and save to reload.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className={Styles.applayout}>
+      <div className={Styles.dashboard}>Dashboard</div>
     </div>
   );
 };
