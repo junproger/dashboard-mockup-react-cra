e3f963d8d60028233bc8f05a5a5074a5e16e7924
Junior Programmer <progphyton@gmail.com>
2023-12-18 19:05:42 +0300

    feat: add usestate and usefetchdata to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,40 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 
 import Styles from './App.module.css';
+import { endpoints } from '../../constants/endpoints';
+import { fetchurl } from '../../constants/fetchurl';
+import { useFetchData } from '../../hooks/useFetchData';
+import { Site, Test } from '../../types/ApiTypes';
+import { logging } from '../../utils/logging';
 import { Dashboard } from '../Dashboard/Dashboard';
 
+export type AppState = {
+  sites: [] | Site[];
+  tests: [] | Test[];
+};
+
 export const App: FC = (): JSX.Element => {
+  const [getState, setState] = useState<AppState>({ sites: [], tests: [] });
+  const CALLDATA = useCallback((point: string, data: Site[] | Test[]) => {
+    if (point === endpoints.sites) {
+      setState((prev) => {
+        return {
+          sites: data as Site[],
+          tests: prev.tests,
+        };
+      });
+    } else if (point === endpoints.tests) {
+      setState((prev) => {
+        return {
+          sites: prev.sites,
+          tests: data as Test[],
+        };
+      });
+    }
+  }, []);
+  useFetchData(fetchurl.distant, endpoints.sites, CALLDATA);
+  useFetchData(fetchurl.distant, endpoints.tests, CALLDATA);
+  logging(getState);
   return (
     <div className={Styles.applayout}>
       <Dashboard />
