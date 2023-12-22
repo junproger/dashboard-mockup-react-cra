1dd9877d7e1ba805802466894816ceeb3d14b7c6
Junior Programmer <progphyton@gmail.com>
2023-12-22 03:06:08 +0300

    feat: level up of sort component and custom hook

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,13 +3,16 @@ import { FC } from 'react';
 import Styles from './Dashboard.module.css';
 import { search } from '../../helpers/search';
 import { useCustomSearch } from '../../hooks/useCustomSearch';
+import { useCustomSort } from '../../hooks/useCustomSort';
 import { DataProp } from '../../types/DataProp';
 import { Loader } from '../Loader/Loader';
 import { Results } from '../Results/Results';
 import { Search } from '../Search/Search';
+import { Sort } from '../Sort/Sort';
 
 export const Dashboard: FC<DataProp> = ({ datasites, datatests }): JSX.Element => {
   const { inputRef, getValue, callvalue, resetvalue } = useCustomSearch();
+  const { callsort, sorteddata, sortstate } = useCustomSort(search(datatests, getValue));
   return (
     <>
       <h1 className={Styles.title}>Dashboard</h1>
@@ -19,8 +22,9 @@ export const Dashboard: FC<DataProp> = ({ datasites, datatests }): JSX.Element =
         resetvalue={resetvalue}
         amount={search(datatests, getValue).length}
       />
+      {!!datatests.length && <Sort callsort={callsort} sortstate={sortstate} />}
       {datatests.length > 0 ? (
-        <Results datasites={datasites} datatests={search(datatests, getValue)} resetvalue={resetvalue} />
+        <Results datasites={datasites} datatests={sorteddata} resetvalue={resetvalue} />
       ) : (
         <Loader />
       )}
